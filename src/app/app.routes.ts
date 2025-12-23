bb0ba08369fddef6740b018f272cb966642f2d7a
Vishmay Dhruv <vishmay.dhruv@tatvasoft.com>
2025-12-23 12:18:21 +0530

    feat: setup layout-based routing architecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './layouts/auth-layout/auth-layout';
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { Login } from './features/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: AuthLayout,
+    children:[
+      {
+        path: '',
+        loadComponent: () => import('./features/auth/login/login.js').then(m => m.Login)
+      }
+    ]
+  },
+  {
+    path: 'app',
+    component: MainLayout,
+    children:[
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./features/dashboard/dashboard/dashboard.js').then(m => m.Dashboard),
+      },
+    ]
+  },
+  {
+    path:'**',
+    redirectTo: 'login'
+  }
+];
